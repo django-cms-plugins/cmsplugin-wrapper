e8118a05097ffb59a0ff2d753ecfd3be67fbaca9
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2012-08-23 20:15:12 +0200

    fix

--- cmsplugin_wrapper/plugin_processors.py
+++ cmsplugin_wrapper/plugin_processors.py
@@ -13,21 +13,21 @@ def wrap_plugin(instance, placeholder, rendered_content, original_context):
             'plugins': [],
             'plugin_counter': 0
         }
-        wrap_holder['at_index'] = wrap_holder.at_index and wrap_holder.at_index + 1 or 0
+        wrap_holder['at_index'] = wrap_holder['at_index'] and wrap_holder['at_index'] + 1 or 0
         wrap_holder['index'].append(wrap_info)
         
-    elif wrap_holder.at_index and not (instance._render_meta.text_enabled and instance.parent):
-         wrap_info = wrap_holder['index'][wrap_holder.at_index]
-         wrap_info['plugin_counter'] = wrap_info['plugin_counter'] + 1
-         wrap_info['plugins'].append((instance, rendered_content)
+    elif wrap_holder['at_index'] and not (instance._render_meta.text_enabled and instance.parent):
+        wrap_info = wrap_holder['index'][wrap_holder.at_index]
+        wrap_info['plugin_counter'] = wrap_info['plugin_counter'] + 1
+        wrap_info['plugins'].append((instance, rendered_content))
          
-         if wrap_info['plugin_counter'] == wrap_info['wrapper_plugin'].number or 
-             wrap_info['plugin_counter'] == instance._render_meta.instance._render_meta.total:
+        if wrap_info['plugin_counter'] == wrap_info['wrapper_plugin'].number or \
+            wrap_info['plugin_counter'] == instance._render_meta.instance._render_meta.total:
              
         template = Template(wrap_info['wrapper_plugin'])
         context = wrap_info['context']
         context['plugins'] = wrap_info['plugins']
         wrap_holder['index'].pop()
-        wrap_holder['at_index'] = wrap_holder['at_index'] - 1
+        wrap_holder['at_index'] = wrap_holder['at_index'] == 0 and None or wrap_holder['at_index'] - 1
         
         template.render(context)
